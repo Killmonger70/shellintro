5ff236b1abe4cb585594eca46020a21ea289677e
Nicolas Ramirez <nico.elfo@gmail.com>
2020-12-20 20:25:01 -0500

    en el punto se le añadio los reads y los echo respectivos al proyecto

--- 3.sh
+++ 3.sh
@@ -1,5 +1,12 @@
 # !/bin/bash
 # Nicolas Ramirez
 # comentarios y userinput en la terminal usualmente
+# principalmente el uso de read y read con -p
 
+read -p "escribe tu nombre porfavor: " NOMBRE
 
+echo "me dijiste que tu nombre era: ${NOMBRE}, cierto?"
+
+read -p "que edad tienes, ${NOMBRE}?" EDAD
+
+echo "${NOMBRE} tiene ${EDAD} años de edad"
