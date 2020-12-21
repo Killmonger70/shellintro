711dc585952fc61affd84bfc625c488601fe35d6
Nicolas Ramirez <nico.elfo@gmail.com>
2020-12-20 20:55:57 -0500

    todo lo de los if y su else, me atormenta el conocer como hacer operaciones aritmeticas ( y de algebra) en la terminal

---
+++ 4.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+# Nicolas Ramirez 
+# vamos a los if y a los else de bash
+
+read -p "que edad tienes?: " AGE
+
+if [ ${AGE} = "17"  ];
+then
+	echo "tienes la misma edad que cuando empeze en bash"
+else
+	echo "te pregunto xdxdxdxd"
+fi
