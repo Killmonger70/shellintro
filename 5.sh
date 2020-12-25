fedd183774b1a2591778611fa024f49bfd01a5e9
Nicolas Ramirez <nico.elfo@gmail.com>
2020-12-24 20:51:06 -0500

    los test, no se usa el comando como tal (solo en el man test), sino los argumentos, esta vez use el argumento -e , para verificar la existencia

---
+++ 5.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+# nicolas ramirez
+# un analisis a los test y su similaridad a una estructura booleana
+
+if [ -e ./hola.txt ];
+then 
+	echo " it exists!!"
+else 
+	echo " it doesn't exist"
+fi
+
