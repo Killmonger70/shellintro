823183ef2f22de6834df90b731c174ca0da6c076
nicolas ramirez <nico.elfo@gmail.com>
2020-12-30 21:06:24 +0000

    segundo proyecto de operaciones aritmeticas / second proyect of mainly arithmetic in bash or terminal

---
+++ proyect2.sh
@@ -0,0 +1,16 @@
+# !/bin/bash
+# Nicolas Ramirez
+# hoy mirare multiples formas de hacer operaciones aritmeticas en la terminal
+
+ADD=$(( 1 + 2))
+echo $ADD
+MUL=$(( $ADD * 5))
+echo $MUL
+REST=$(( $MUL - 5))
+echo $REST
+DIV=$(( $REST / 2))
+echo $DIV
+RESI=$(( $DIV % 2))
+echo $RESI
+
+
