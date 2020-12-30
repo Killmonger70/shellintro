43a5fb10bf38cb1bdc36776a07a353af698234f4
nicolas ramirez <nico.elfo@gmail.com>
2020-12-30 20:50:16 +0000

    un for loop con la utilidad de un cat

--- 8.sh
+++ 8.sh
@@ -1,5 +1,12 @@
 # !/bin/bash
 # Nicolas ramirez
-# Los ciclos for de nuevo
+# Los ciclos for de nuevo / the for loops again
+
+#for Var in $(); do
+#  command
+#done                    <-- this is the basic syntax
 
 
+for NAMES in $(cat names.txt); do
+  echo "the names are: $NAMES"
+done
