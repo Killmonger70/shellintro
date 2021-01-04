c67f3d7be2dbac73a0e6cba1d48b45b8549077df
nicolas ramirez <nico.elfo@gmail.com>
2021-01-04 20:01:50 +0000

    finalizacion de el proyecto 2 e inicio de un proyecto awk / the finalization of proyect 2 and the start of an awk proyect

--- proyect2.sh
+++ proyect2.sh
@@ -27,7 +27,27 @@ expr 8 != 5
 expr 8 \> 5
 expr 8 \< 5
 expr 8 \<= 5
-echo ""
 ADD=$(expr $ADD + 1) && echo $ADD
+echo ""
+
+# using the bc command / usando el comando bc
+# util para operaciones de floating math / useful for floating math operations
+# you must install it first / ha de ser instalado manualmente primero
+# es en modo prompt , o como un comando para enviar a un pipe
+# it is in prompt mode , it can also be used by using a pipe
+echo '3+5' | bc 
+echo '15 % 2' | bc
+echo '15 / 2' | bc
+echo '(6 * 2) - 5' | bc
+# it can also use decimal results with the -l flag
+# tambien puede usar resultados decimales con -l
+echo '15 / 2' | bc -l
+echo ""
+
+# el comando awk / the awk command
+# awk is widely seen in learning platforms like youtube as a text processing programm
+# it can also be considered a language by itself
+# awk es bastante visto en youtube como un programa de procesamiento de texto
+
 
 
